e8d07175c8a3f613889473115a046fb61fda7544
minwkim <mkim8@uvm.edu>
2015-04-15 19:55:00 -0400

    first

---
+++ GitHubTest.java
@@ -0,0 +1,7 @@
+class GitHubTest{
+   public static void main(String [] args){
+      System.out.println("You got to GitHub!!!");
+      System.out.println("Now making a change.");
+      System.out.println("crossing fingers");
+   }
+}
