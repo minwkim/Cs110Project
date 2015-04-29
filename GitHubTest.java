9b4c488059a5aacbbdc47ef29825f21d5b3655b6
minwkim <mkim8@uvm.edu>
2015-04-29 05:47:20 -0400

    Changes test

--- GitHubTest.java
+++ GitHubTest.java
@@ -2,6 +2,6 @@ class GitHubTest{
    public static void main(String [] args){
       System.out.println("You got to GitHub!!!");
       System.out.println("Now making a change.");
-      System.out.println("crossing fingers");
+      System.out.println("crossing fingrs");
    }
 }
